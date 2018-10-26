74302d1c60deeee5428d20dd1425d8584d776a75
BieLuk <lukcxz1@wp.pl>
2018-10-26 18:58:01 +0200

    Initial commit

---
+++ src/main/java/invoice/test/HelloController.java
@@ -0,0 +1,13 @@
+package invoice.test;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String sayHi() {
+        return "Hi";
+    }
+}
---
+++ src/main/java/invoice/test/TopicController.java
@@ -0,0 +1,28 @@
+package invoice.test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class TopicController {
+
+    @Autowired
+    private TopicService topicService;
+
+    @RequestMapping("/topics")
+    public List<Topic> getAllTopics() {
+        return topicService.getAllTopics();
+    }
+
+    @RequestMapping("/topics/{id}")
+    public Topic getTopic(@PathVariable String id){
+        return topicService.getTopic(id);
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/topics") //sprawdzic czy postmapping bedzie lepsze
+    public void addTopic(@RequestBody Topic topic) {
+        topicService.addTopic(topic);
+    }
+}
